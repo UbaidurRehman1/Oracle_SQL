f517977b937c6d2b5afe8d480a492ece32ff1886
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-08-01 22:50:50 +0500

    scripts add

--- employeesManagement.sql
+++ employeesManagement.sql
@@ -1,18 +1,17 @@
-drop table "SYSTEM"."REGIONS"; 
+--------------------------Creating All Tables----------------------------------
 CREATE TABLE regions
 (
     region_id INT PRIMARY KEY,
     region_name VARCHAR(255)
 );
-drop table "SYSTEM"."countries"; 
+
 create table countries
 (
     country_id int primary key,
     country_name varchar(255),
     region_id int,
-    constraint fk_cout_reg foreign key (countries) REFERENCES regions(region_id)
+    constraint fk_cout_regions foreign key (country_id) REFERENCES regions(region_id)
 );
-drop table "SYSTEM"."locations"; 
 create table locations
 (
     location_id int primary key,
@@ -23,7 +22,6 @@ create table locations
     country_id int,
     constraint fk_loc_country foreign key(country_id) references countries(country_id)
 );
-drop table "SYSTEM"."jobs"; 
 create table jobs
 (
     job_id int primary key,
@@ -31,16 +29,14 @@ create table jobs
     min_salary int,
     max_salary int
 );
-drop table "SYSTEM"."departments"; 
 create table departments
 (
     department_id int primary key,
     department_name varchar(255),
     manager_id int,
     location_id int,
-    constraint fk_dept_loc foreign key(locations) references locations(location_id)
+    constraint fk_dept_loc foreign key(location_id) references locations(location_id)
 );
-drop table "SYSTEM"."employees"; 
 create table employees
 (
     employee_id int primary key,
@@ -55,18 +51,17 @@ create table employees
     manager_id int,
     department_id int,
     constraint fk_employees_job foreign key(job_id) references jobs(job_id),
-    constraint fk_emp_dept foreign key(deparment_id) references departments(department_id)
+    constraint fk_emp_dept foreign key(department_id) references departments(department_id)
     
 );
-drop table "SYSTEM"."job_history"; 
 create table job_history
 (
     employee_id int primary key,
     start_data date,
-    end_date data,
+    end_date date,
     job_id int,
     department_id int,
     constraint fk_job_history_job foreign key (job_id) references jobs(job_id),
-    constraint fk_job_history_emp foreign key (employee_id) references employees (employee_id),
-    constraint fk_job_history_dept foreign key (department_id) REFERENCES ddepartments(department_id)
+    constraint fk_job_history_emp foreign key (employee_id) references employees(employee_id),
+    constraint fk_job_history_dept foreign key (department_id) REFERENCES departments(department_id)
 );
